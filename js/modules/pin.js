1e85594ccdcb6cd56c52625e1ee9988a1570b6e1
Alexander <borom@inbox.ru>
2017-12-11 16:28:49 +0300

    Доработан модуль pin

--- js/modules/pin.js
+++ js/modules/pin.js
@@ -1,13 +1,25 @@
 ﻿'use strict';
 
 window.pin = (function () {
+  var width = 20;
+  var heigth = 44;
+
+  function render(arrayAvatars, creatingFunctionName) {
+    var mapList = document.querySelector('.map__pins');
+    var fragment = document.createDocumentFragment();
+    for (var i = 0; i < arrayAvatars.length; i++) {
+      fragment.appendChild(creatingFunctionName(arrayAvatars[i], i));
+    }
+    mapList.appendChild(fragment);
+  }
+
   return {
     renderMapPins: function (arr) {
-      function createMapPin(arrAvatars) {
+      function createElement(arrAvatars) {
         var newMapPin = document.createElement('button');
         newMapPin.className = 'map__pin';
-        newMapPin.style.left = (arrAvatars.location.x + 20) + 'px';
-        newMapPin.style.top = (arrAvatars.location.y + 44) + 'px';
+        newMapPin.style.left = (arrAvatars.location.x + width) + 'px';
+        newMapPin.style.top = (arrAvatars.location.y + heigth) + 'px';
         var newAvatar = document.createElement('img');
         newAvatar.width = 40;
         newAvatar.heigth = 40;
@@ -16,16 +28,7 @@ window.pin = (function () {
         newMapPin.appendChild(newAvatar);
         return newMapPin;
       }
-
-      function renderMapPins(arrayAvatars, creatingFunctionName) {
-        var mapList = document.querySelector('.map__pins');
-        var fragment = document.createDocumentFragment();
-        for (var i = 0; i < arrayAvatars.length; i++) {
-          fragment.appendChild(creatingFunctionName(arrayAvatars[i], i));
-        }
-        mapList.appendChild(fragment);
-      }
-      renderMapPins(arr, createMapPin);
+      render(arr, createElement);
     }
   };
 })();
